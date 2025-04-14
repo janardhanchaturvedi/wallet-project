4eb56f699b4517f491298cd30704416a5dc90b08
Janardhan <jan.cj84@gmail.com>
2025-04-14 17:32:37 +0530

    feat: Implement user dashboard and transaction pages, enhance UI components, and integrate webhook processing

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,11 +1,46 @@
 import express from "express";
-
+import db from "@repo/db/client";
 const app = express();
 
-app.post("/hdfcWebhook", (req, res) => {
+app.post("/hdfcWebhook", async (req, res) => {
   const paymentInformation = {
     token: req.body.token,
     userId: req.body.user_identifier,
     amount: req.body.amount,
   };
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInformation.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.json({
+      message: "Captured",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
