94eff86ccd8af85fc6b54e6bc79190c37341074d
Janardhan <jan.cj84@gmail.com>
2025-04-14 21:55:45 +0530

    feat : Added the create on OnRampTransaction action and integrated the add money

--- apps/user-app/app/lib/actions/createOnRampTransaction.ts
+++ apps/user-app/app/lib/actions/createOnRampTransaction.ts
@@ -1,3 +1,34 @@
-export function CreateOnRampTransaction() {
-    
-}    
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function CreateOnRampTransaction(
+  provider: string,
+  amount: number
+) {
+  const session = await getServerSession(authOptions);
+  // This token will ideally come from the hdfc server to corresponding amount
+  const token = Math.random().toString();
+  const userId = session?.user?.id;
+  if (!userId) {
+    return {
+      message: "User not logged in",
+    };
+  }
+  await prisma.onRampTransaction.create({
+    data: {
+      userId: Number(userId),
+      amount: amount,
+      status: "Processing",
+      startTime: new Date(),
+      provider,
+      token: token,
+    },
+  });
+
+  return {
+    messages: "On ramp transcation added",
+  };
+}
